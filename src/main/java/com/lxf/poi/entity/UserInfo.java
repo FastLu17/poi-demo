2c3d2758559244ad32789b0a87e7176cf5b75f27
xiao66 <948632050@qq.com>
2019-08-11 22:32:22 +0800

    完善POI-word的操作,实现异步海量数据导出到Word的表格中。

---
+++ src/main/java/com/lxf/poi/entity/UserInfo.java
@@ -0,0 +1,20 @@
+package com.lxf.poi.entity;
+
+import java.io.Serializable;
+import javax.persistence.*;
+import lombok.Data;
+
+@Data
+@Table(name = "`user_info`")
+public class UserInfo implements Serializable {
+    @Column(name = "`name`")
+    private String name;
+
+    @Column(name = "`age`")
+    private Integer age;
+
+    @Column(name = "`address`")
+    private String address;
+
+    private static final long serialVersionUID = 1L;
+}
---
+++ src/main/java/com/lxf/poi/mapper/UserInfoMapper.java
@@ -0,0 +1,11 @@
+package com.lxf.poi.mapper;
+
+import com.lxf.poi.entity.UserInfo;
+import tk.mybatis.mapper.common.Mapper;
+
+import java.util.List;
+import java.util.Map;
+
+public interface UserInfoMapper extends Mapper<UserInfo> {
+    List<Map<String, Object>> selectAllResultMap();
+}
