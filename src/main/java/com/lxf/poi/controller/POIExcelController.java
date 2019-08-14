cee11ebead5aeea2dcd3e8f64d5e301450bd152f
xiao66 <948632050@qq.com>
2019-08-14 11:43:22 +0800

    新增Excel的部分操作：DataFormat、Formula、Comment、PrintSet等。

--- src/main/java/com/lxf/poi/controller/POIExcelController.java
+++ src/main/java/com/lxf/poi/controller/POIExcelController.java
@@ -23,7 +23,9 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.net.URL;
+import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -51,41 +53,50 @@ public class POIExcelController {
         HSSFDataFormat format = workbook.createDataFormat();//格式化
 
         HSSFSheet sheet = workbook.createSheet("sheet");
-        sheet.setSelected(false);
-        HSSFSheet sheetSelected = workbook.createSheet("sheetSelected");
-        sheetSelected.setSelected(true);//设置默认被选中、没生效
+        sheet.setSelected(true);//设置默认被选中、没生效
+
+        HSSFSheet selectedSheet = workbook.createSheet("selectedSheet");
+        selectedSheet.setDisplayGridlines(false);//隐藏Excel网格线,默认值为true
+        selectedSheet.setGridsPrinted(true);//打印时显示网格线,默认值为false
+        workbook.setActiveSheet(1);//设置默认工作表
 
         CellRangeAddress range = CellRangeAddress.valueOf("C5:F20");//不同的方式创建range、
         CellRangeAddress rangeAddress = new CellRangeAddress(1, 1, 1, 1);
-        sheet.setAutoFilter(rangeAddress);//设置自动过滤、
+        selectedSheet.setAutoFilter(rangeAddress);//设置自动过滤、
 
         //设置缩放比例
-        sheet.setZoom(115);// 表示:115%
+        selectedSheet.setZoom(115);// 表示:115%
 
-        sheet.createFreezePane(1, 1);//设置冻结窗格--行和列(不受滚动影响)
+        selectedSheet.createFreezePane(1, 1);//设置冻结窗格--行和列(不受滚动影响)
 
-        //设置头尾(没效果)
-        HSSFHeader header = sheet.getHeader();
-        HSSFFooter footer = sheet.getFooter();
+        //设置头尾(没效果-->应该是Excel设置了不展示头尾)
+        HSSFHeader header = selectedSheet.getHeader();
+        HSSFFooter footer = selectedSheet.getFooter();
         header.setRight(HSSFHeader.font("Stencil-Normal", "Italic") +
                 HSSFHeader.fontSize((short) 16) + "Right w/ Stencil-Normal Italic font and size 16");
         footer.setRight("Footer Right");
 
-        Row row = sheet.createRow(1);
+        Row row = selectedSheet.createRow(1);
         /*
-         *   RowHeightInPoints = 12.75
+         *   RowHeightInPoints = 12.75(磅)
          *   RowHeight = 255
          *   20倍的关系、
+         *   defaultColumnWidth = 8(字符)  1字符≈5.55磅、
          * */
-        float defaultHeightInPoints = sheet.getDefaultRowHeightInPoints();//获取默认行高、
-        sheet.autoSizeColumn(1);//设置某一列自根据内容自动调整宽度、
+        float defaultHeightInPoints = selectedSheet.getDefaultRowHeightInPoints();//获取默认行高、
+        selectedSheet.autoSizeColumn(1);//设置某一列自根据内容自动调整宽度、(默认不会自动换行,需要设置style.setWrapText(true);)
         row.setHeightInPoints(2 * defaultHeightInPoints);//设置两倍行高、
+        int defaultColumnWidth = selectedSheet.getDefaultColumnWidth();//8 -->8个字符、
+        System.out.println("defaultColumnWidth = " + defaultColumnWidth);
+
+        //TODO: 这个参数的单位是1/256个字符宽度 -->表示设置为3个字符宽度、
+        selectedSheet.setColumnWidth(1, 3 * 256);
 
         //设置打印区域
         workbook.setPrintArea(0, 0, 9, 0, 9);
 
-        Font font = getFont(workbook, 200, "Consolas", IndexedColors.RED.index);
-        Font font2 = getFont(workbook, 300, "黑体", IndexedColors.GREEN.index);
+        Font font = getFont(workbook, 10, "Consolas", IndexedColors.RED.index);
+        Font font2 = getFont(workbook, 15, "黑体", IndexedColors.GREEN.index);
 
         //富文本、
         HSSFRichTextString richString = new HSSFRichTextString("Hello, World!");
@@ -164,6 +175,8 @@ public class POIExcelController {
     public void utils() throws Exception {
         HSSFWorkbook workbook = new HSSFWorkbook();
         HSSFSheet sheet = workbook.createSheet();
+        //设置自动根据内容扩展列宽
+        sheet.autoSizeColumn(3);
 
         CellRangeAddress rangeAddress = CellRangeAddress.valueOf("C5:F20");
 //        CellRangeAddress rangeAddress = new CellRangeAddress(1, 9, 1, 9);
@@ -180,6 +193,15 @@ public class POIExcelController {
         style.setAlignment(HorizontalAlignment.CENTER);
         style.setVerticalAlignment(VerticalAlignment.CENTER);
 
+        //添加日期格式化的样式、(两种数据格式化方式)
+        HSSFDataFormat dataFormat = workbook.createDataFormat();
+        style.setDataFormat(dataFormat.getFormat("yyyy-mm-dd h:mm:ss"));
+//        style.setDataFormat(HSSFDataFormat.getBuiltinFormat("yyyy-mm-dd"));
+
+        //TODO: 可以通过打印或者源码查看 默认的格式、
+        List<String> builtinFormats = HSSFDataFormat.getBuiltinFormats();
+        builtinFormats.forEach(System.out::println);
+
         HSSFRow row = sheet.createRow(0);
         HSSFRow row1 = sheet.createRow(1);
         Cell cell_00 = CellUtil.createCell(row, 0, "CellUtil--00", style);
@@ -187,7 +209,41 @@ public class POIExcelController {
         Cell cell_10 = CellUtil.createCell(row1, 0, "CellUtil--10", style);
         Cell cell_11 = CellUtil.createCell(row1, 1, "CellUtil--11", style);
 
+
+        double excelDate = HSSFDateUtil.getExcelDate(new Date());
+        Date javaDate = HSSFDateUtil.getJavaDate(excelDate);
+
+        System.out.println("excelDate = " + excelDate);
+        System.out.println("javaDate = " + javaDate);
+
+        HSSFCell cell = row.createCell(3);
+        cell.setCellStyle(style);//此单元格应用了日期格式化的样式、
+        cell.setCellValue(new Date());
+        HSSFCell cell4 = row.createCell(4);
+        cell4.setCellValue(excelDate);//日期需要格式化、否则无法正常显示、
+
         FileOutputStream outputStream = new FileOutputStream(BASE_DIRECTORY_PATH + "HSSF测试utils.xls");
+        workbook.write(outputStream);
+
+        excelUtil.closeStream(workbook, outputStream);
+    }
+
+    /**
+     * 为单元格添加计算公式、
+     */
+    @GetMapping("formula")
+    public void formula() throws Exception {
+        HSSFWorkbook workbook = new HSSFWorkbook();
+        HSSFSheet sheet = workbook.createSheet();
+        HSSFRow row = sheet.createRow(0);
+        HSSFCell cell = row.createCell(0);
+        cell.setCellFormula("2+3*4");//设置公式
+        cell = row.createCell(1);
+        cell.setCellValue(10);
+        cell = row.createCell(2);
+        cell.setCellFormula("A1*B1");//设置公式 "sum(A1,C1)" "sum(B1:D1)" 等Excel函数都可以使用、
+
+        FileOutputStream outputStream = new FileOutputStream(BASE_DIRECTORY_PATH + "HSSF测试formula.xls");
         workbook.write(outputStream);
 
         excelUtil.closeStream(workbook, outputStream);
@@ -224,7 +280,7 @@ public class POIExcelController {
         HSSFTextbox textBox = patriarch.createTextbox(anchorTextBox);
         textBox.setLineStyle(HSSFShape.LINESTYLE_SOLID);
         HSSFRichTextString richTextString = new HSSFRichTextString("This is a test");
-        richTextString.applyFont(getFont(workbook, 200, "Consolas", IndexedColors.RED.index));
+        richTextString.applyFont(getFont(workbook, 10, "Consolas", IndexedColors.RED.index));
         textBox.setString(richTextString);
         textBox.setVerticalAlignment(HSSFTextbox.VERTICAL_ALIGNMENT_CENTER);
         textBox.setHorizontalAlignment(HSSFTextbox.VERTICAL_ALIGNMENT_CENTER);
@@ -269,7 +325,10 @@ public class POIExcelController {
         int pictureIndex = workbook.addPicture(bytes, Workbook.PICTURE_TYPE_JPEG);
         HSSFClientAnchor anchorPicture = new HSSFClientAnchor(0, 0, 1023, 255, (short) 7, 1, (short) 8, 3);
         //开始画图片、
-        patriarch.createPicture(anchorPicture, pictureIndex);
+        HSSFPicture picture = patriarch.createPicture(anchorPicture, pictureIndex);
+
+        //设置图片
+        picture.resize();//自动调节图片大小,图片位置信息可能丢失
         read.flush();
 
         FileOutputStream outputStream = new FileOutputStream(BASE_DIRECTORY_PATH + "HSSF测试pictures.xls");
@@ -279,21 +338,52 @@ public class POIExcelController {
     }
 
     /**
-     *  超链接引入图片、
+     * 利用HSSFPatriarch对象,创建批注、注释.
+     */
+    @GetMapping("comment")
+    public void comment() throws Exception {
+        HSSFWorkbook workbook = new HSSFWorkbook();
+        HSSFSheet sheet = workbook.createSheet();
+        HSSFPatriarch patriarch = sheet.createDrawingPatriarch();
+        HSSFClientAnchor anchor = patriarch.createAnchor(0, 0, 0, 0, 5, 1, 8, 3);//创建批注位置(锚)
+        HSSFComment comment = patriarch.createCellComment(anchor);//创建批注
+        comment.setString(new HSSFRichTextString("这是一个批注段落！"));//设置批注内容
+        comment.setAuthor("小66");//设置批注作者
+//        comment.setFillColor(IndexedColors.RED.index);
+//        comment.setBackgroundImage();//背景图片
+        comment.setVisible(false);//设置批注 默认显示-->true
+        HSSFCell cell = sheet.createRow(2).createCell(1);
+        cell.setCellValue("新建批注");
+        cell.setCellComment(comment);//把批注赋值给单元格
+
+        FileOutputStream outputStream = new FileOutputStream(BASE_DIRECTORY_PATH + "HSSF测试comment.xls");
+        workbook.write(outputStream);
+
+        excelUtil.closeStream(workbook, outputStream);
+    }
+
+    /**
+     * 超链接引入图片、
      */
     @GetMapping("hyperlink")
-    public void hyperlink()throws Exception{
+    public void hyperlink() throws Exception {
         HSSFWorkbook workbook = new HSSFWorkbook();
+        /*
+         *   HyperlinkType.URL：关联到指定Url、
+         *   HyperlinkType.FILE：关联到目录文件、
+         *   HyperlinkType.DOCUMENT：工作簿中的位置、
+         * */
         HSSFHyperlink hyperlink = workbook.getCreationHelper().createHyperlink(HyperlinkType.URL);
         hyperlink.setShortFilename("驴子");
         hyperlink.setAddress("http://p20.qhimgs3.com/dr/240_240_/t011c15f8c12e731c01.jpg?t=1558300397");
         HSSFSheet sheet = workbook.createSheet();
         HSSFCell cell = sheet.createRow(0).createCell(0);
         HSSFCellStyle style = workbook.createCellStyle();
-        Font font = getFont(workbook, 200, "Consolas", IndexedColors.BLUE.index);
+        Font font = getFont(workbook, 10, "Consolas", IndexedColors.BLUE.index);
         style.setFont(font);
+        cell.setCellStyle(style);//为单元格设置样式,否则样式不生效、
         cell.setHyperlink(hyperlink);
-        cell.setCellValue("图片链接");
+        cell.setCellValue("hyperlink");
 
         FileOutputStream outputStream = new FileOutputStream(BASE_DIRECTORY_PATH + "HSSF测试hyperlink.xls");
         workbook.write(outputStream);
@@ -352,6 +442,30 @@ public class POIExcelController {
         excelUtil.closeStream(workbook, outputStream);
     }
 
+    /**
+     * 打印设置、PrintSetup
+     */
+    @GetMapping("printSet")
+    public void printSet() {
+        HSSFWorkbook workbook = new HSSFWorkbook();
+        HSSFSheet sheet = workbook.createSheet("Test0");// 创建工作表(Sheet)
+        HSSFPrintSetup print = sheet.getPrintSetup();//得到打印对象
+        print.setLandscape(false);//true，则表示页面方向为横向；否则为纵向
+        print.setScale((short) 80);//缩放比例80%(设置为0-100之间的值)
+        print.setFitWidth((short) 2);//设置页宽
+        print.setFitHeight((short) 4);//设置页高
+        print.setPaperSize(HSSFPrintSetup.A4_PAPERSIZE);//纸张设置
+        print.setUsePage(true);//设置打印起始页码不使用"自动"
+        print.setPageStart((short) 6);//设置打印起始页码
+        sheet.setPrintGridlines(true);//设置打印网格线
+        print.setNoColor(true);//值为true时，表示单色打印
+        print.setDraft(true);//值为true时，表示用草稿品质打印
+        print.setLeftToRight(true);//true表示“先行后列”；false表示“先列后行”
+        print.setNotes(true);//设置打印批注
+        sheet.setAutobreaks(false);//Sheet页自适应页面大小
+
+    }
+
     private void createCell(Workbook wb, Font font, Row row, int column, HorizontalAlignment halign, VerticalAlignment valign, String value) {
         Cell cell = row.createCell(column);
         cell.setCellValue(value);
@@ -371,15 +485,19 @@ public class POIExcelController {
         cellStyle.setBorderTop(BorderStyle.MEDIUM_DASHED);
         cellStyle.setTopBorderColor(IndexedColors.RED.getIndex());
 
-        //设置填充属性
-//        cellStyle.setFillBackgroundColor(IndexedColors.AQUA.getIndex());
-//        cellStyle.setFillPattern(FillPatternType.BIG_SPOTS);
+        //设置填充属性(背景和纹理):-->不常用、
+//        cellStyle.setFillForegroundColor(HSSFColor.GREEN.index);//设置图案颜色
+//        cellStyle.setFillBackgroundColor(HSSFColor.RED.index);//设置图案背景色
+//        cellStyle.setFillPattern(FillPatternType.BIG_SPOTS);//设置图案样式
 
         //设置字体属性
         cellStyle.setFont(font);
 
         //设置换行
         cellStyle.setWrapText(true);
+
+        //设置锁定、
+        cellStyle.setLocked(true);
 
         cell.setCellStyle(cellStyle);
     }
@@ -425,17 +543,16 @@ public class POIExcelController {
      * 不要循环创建字体样式,尽量重用、
      *
      * @param workbook
-     * @param fontHeight 类似字体大小、但是与fontSize不一样。 200 height <==> 10 size
-     * @param fontName   字体名称
-     * @param color      eg:IndexedColors.GREEN.index,HSSFColor.RED.index
+     * @param fontHeightInPoints 字号大小。 200 FontHeight <==> 10 FontHeightInPoints
+     * @param fontName           字体名称
+     * @param color              eg:IndexedColors.GREEN.index,HSSFColor.RED.index
      * @return
      */
-    public Font getFont(Workbook workbook, int fontHeight, String fontName, short color) {
+    public Font getFont(Workbook workbook, int fontHeightInPoints, String fontName, short color) {
         Font font = workbook.createFont();
-//        font.setFontHeightInPoints((short)500);//目前没看到效果
+        font.setFontHeightInPoints((short) fontHeightInPoints);//设置字号、
         font.setFontName(fontName);
         font.setBold(true);
-        font.setFontHeight((short) fontHeight);
         font.setItalic(true);//斜体
 //        font.setStrikeout(true);//删除线
         font.setUnderline(Font.U_SINGLE);
